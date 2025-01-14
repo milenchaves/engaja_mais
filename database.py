1e4ae63abcbbc90ef863c52e30e3e4a6cf502951
milenchaves <milenchaves.study@gmail.com>
2025-01-14 16:05:31 -0300

    pasta models e routes

---
+++ database.py
@@ -0,0 +1,31 @@
+import sqlite3
+from sqlmodel import create_engine, Session, SQLModel
+from sqlalchemy import event, Engine
+from dotenv import load_dotenv
+import logging
+import os
+
+# Carregar variáveis do arquivo .env
+load_dotenv()
+
+# Configurar o logger
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
+# Configuração do banco de dados
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+# Criar a(s) tabela(s) no banco de dados
+# Inicializa o banco de dados
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Session:
+    return Session(engine)
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if type(dbapi_connection) is sqlite3.Connection:  # somente para o SQLite
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
