bf0f3f7a54c8dc15d5b2a68577640b489e6568a8
milenchaves <milenchaves.study@gmail.com>
2025-01-14 21:06:57 -0300

    models inscricao e organizacao

---
+++ models/inscricao.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .vaga import Vaga, VagaBase
+    from .voluntario import Voluntario
+
+class InscricaoBase(SQLModel):
+    id: int | None = Field(default=None, primary_key=True)
+    status: str
+    
+class Inscricao(InscricaoBase, True):
+    id_vaga: int = Field(default=None, foreign_key=True)
+    id_voluntario: int = Field(default=None, foreign_key=True)
+    vaga: 'Vaga' = Relationship(back_populates="inscricao")
+    voluntario: 'Voluntario' = Relationship(back_populates="inscricao")
+
+class IncricaoComVoluntario(InscricaoBase):
+    vaga: VagaBase | None
+    inscritos: list[Voluntario] = None
--- models/vaga.py
+++ models/vaga.py
@@ -1,19 +1,20 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List, TYPE_CHECKING
-from datetime import date
-
+from datetime import datetime, timezone
 if TYPE_CHECKING:
-    from .inscricao import Inscricao
-    from .organizacao import Organizacao
+    from .organizacao import Organizacao, OrganizacaoBase
 
 class VagaBase(SQLModel):
-    id_vaga: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     nome_vaga: str
     descricao_vaga: str
-    data_publicacao: date
+    data_publicacao: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     status_vaga: str
 
 class Vaga(VagaBase, table=True):
-    id_organizacao: int = Field(foreign_key="organizacao.id_organizacao")
-    inscricoes: List["Inscricao"] = Relationship(back_populates="vaga")
-    organizacao: "Organizacao" = Relationship(back_populates="vagas")
+    id_organizacao: int = Field(foreign_key="organizacao.id")
+    organizacao: 'Organizacao' = Relationship(back_populates="vagas")
+
+class VagaComOrganizacao(VagaBase):
+    organizacao: OrganizacaoBase | None
+
